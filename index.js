bf04a513599d7e5bb9341872b049198340ae50ec
Gaurav Pal <gaurav.pal1142@gmail.com>
2022-10-17 19:24:46 +0530

    Seventh commit (dark mode final edit)

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function closeCoupon() {
   document.getElementById("coupon").style.visibility = "hidden";
 }
 function darkmode() {
+  var toggleicon = document.getElementById("toggle-icon");
   document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    toggleicon.classList.remove("fa-moon");
+    toggleicon.classList.add("fa-sun");
+  } else {
+    toggleicon.classList.remove("fa-sun");
+    toggleicon.classList.add("fa-moon");
+  }
 }
